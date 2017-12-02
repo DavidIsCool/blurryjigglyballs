acae2243ab8c4e0082083c181e5ce0e2a0105a5b
AndrewDongAndy <dongandrew99@gmail.com>
2017-12-02 11:09:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+var numMoves = 0;
+let icons = ['X', 'O']
+let firstMover = 'X'
+
+function toMove(numMoves) {
+    return icons[numMoves % 2]
+}
+
 function move(btnid) {
-    document.getElementById("ttt" + btnid).value = "x"
+    document.getElementById('ttt' + btnid).value = toMove()
+    numMoves++
+    document.getElementById('tttToMove').value = `To move: ${toMove()}`
 }
 
