6a408b2876d4362e0c264abdbb062710556c15d7
Kevin Dong <dongkl@hotmail.com>
2017-12-02 17:23:25 -0500

    Renamed testRow to testLine, consistency of boolean checking

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 let numMoves = 0;
 const icons = ['X', 'O'];
 var grid = ['', '', '', '', '', '', '', '', '']
-let result;
 
 function toMove(numMoves) {
     return icons[numMoves % 2];
 }
 
-function testRow(a, b, c) {
-    return ((grid[a] === grid[b]) && (grid[b] === grid[c]) && grid[a] !== '')
+function testLine(a, b, c) {
+    return (grid[a] === grid[b] && grid[b] === grid[c] && grid[a] !== '')
 }
 
 function hasThreeInARow() {
-    return (testRow(0, 1, 2) || testRow(3, 4, 5) ||
-    testRow(6, 7, 8) || testRow(0, 3, 6) || testRow(1, 4, 7) ||
-    testRow(2, 5, 8) || testRow(0, 4, 8) || testRow(2, 4, 6))
+    return (testLine(0, 1, 2) || testLine(3, 4, 5) ||
+    testLine(6, 7, 8) || testLine(0, 3, 6) || testLine(1, 4, 7) ||
+    testLine(2, 5, 8) || testLine(0, 4, 8) || testLine(2, 4, 6))
 }
 
 function disableAllSquares() {
