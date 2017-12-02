af9d5b7dcffb8f3d1a8c3f57bceedb451870e432
DavidIsCool <davexiao2002@gmail.com>
2017-12-02 17:26:16 -0500

    lol

--- agar.js
+++ agar.js
@@ -1,13 +1,20 @@
+// Variable Declaration
 var canvas = document.getElementById('canvas');
+var canvasFood = document.getElementById('canvasFood');
 var context = canvas.getContext('2d');
+var ctx = canvasFood.getContext('2d');
+
 var x = canvas.width/2;
 var y = canvas.height/2;
 var r = 3;
 
+var ranx = Math.floor(Math.random() * 295);
+var rany = Math.floor(Math.random() * 147);
 
 var mouseX;
 var mouseY;
 
+// animated frames
 var requestAnimationFrame = window.requestAnimationFrame || 
                             window.mozRequestAnimationFrame || 
                             window.webkitRequestAnimationFrame || 
@@ -15,7 +22,7 @@ var requestAnimationFrame = window.requestAnimationFrame ||
 
 
 
-
+// tracks mouse coordinates
 function  mouseTrack(evt) {
   	var rect = canvas.getBoundingClientRect(), // abs. size of element
       scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
@@ -28,11 +35,25 @@ function  mouseTrack(evt) {
     document.getElementById("demo").innerHTML = coor;
 }
     
+// generates food
+function food() {
+	ctx.beginPath();
+	ctx.arc(ranx, rany, 1, 0, 2 * Math.PI, false);
+	ctx.fillStyle = 'red';
+	ctx.fill();
+	ctx.closePath();
+}
 
+// erases food
+function erase(c, ex, ey, rad){
+    //c.globalCompositeOperation = 'destination-out'
+    //c.arc(ex, ey, rad, 0, Math.PI*2, true);
+   // c.fill();
+    c.clearRect(0, 0, canvasFood.width, canvasFood.height);
+}
 
-
-
-function circle() {
+// player logic
+function player() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.beginPath();
 	context.arc(x, y, r, 0, 2 * Math.PI, false);
@@ -40,7 +61,8 @@ function circle() {
 	context.fill();
 	context.closePath(); 
 	document.getElementById("rip").innerHTML = "Coordinates: (" + x + "," + y + ")";
-	
+	document.getElementById("food").innerHTML = "Coordinates: (" + ranx + "," + rany + ")";
+	// movement logic
 	if (x > mouseX) {
 		x -= 1;
 	} else if (x < mouseX) {
@@ -59,13 +81,24 @@ function circle() {
 	if (x < (mouseX + 10) && x > (mouseX - 10)) {
 		x = x;
 	}
-
 	
-	requestAnimationFrame(circle);
+	// check if food has been eaten
+	if (x < (ranx + 4) && x > (ranx - 4) && y > (rany - 4) && y < (mouseY + 4)) {
+		erase(ctx, ranx, rany, 1);
+		r += 0.8;
+		ranx = Math.floor(Math.random() * 295);
+		rany = Math.floor(Math.random() * 147);
+		food();
+		
+		
+		
+	}
+	
+	requestAnimationFrame(player);
 }
 
-
-circle();
+food();
+player();
 
 
 
