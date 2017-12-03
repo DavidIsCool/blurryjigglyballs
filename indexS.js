c2563efcf86a535904190d32a1ef920257b5d5bb
Kevin Dong <dongkl@hotmail.com>
2017-12-02 19:25:15 -0500

    Added vertical line checking (forgot before)

--- indexS.js
+++ indexS.js
@@ -2,10 +2,11 @@
 
 let numMoves = 0;
 const icons = ['X', 'O'];
-var grid = ['', '', '', '', '', '', '', '', '', '', '', ''] // 12 boxes
+var grid = ['', '', '', '', '', '', '', '', '', '', '', '']; // 12 boxes
 const tests = [[0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 0, 1],
 [4, 5, 6], [5, 6, 7], [6, 7, 4], [7, 4, 5],
 [8, 9, 10], [9, 10, 11], [10, 11, 8], [11, 8, 9],
+[0, 4, 8], [1, 5, 9], [2, 6, 10], [3, 7, 11],
 [0, 5, 10], [1, 6, 11], [2, 7, 8], [3, 4, 9],
 [0, 7, 10], [1, 4, 11], [2, 5, 8], [3, 6, 9]];
 let toHighlight = [];
@@ -15,17 +16,18 @@ function toMove(numMoves) {
 }
 
 function isLine(a, b, c) {
-    return (grid[a] === grid[b] && grid[b] === grid[c] && grid[a] !== '')
+    return (grid[a] === grid[b] && grid[b] === grid[c] && grid[a] !== '');
 }
 
 function newGame() {
     numMoves = 0;
     grid = ['', '', '', '', '', '', '', '', '', '', '', ''];
+    toHighlight = [];
     document.getElementById('tttToMove').innerHTML = 'To Move: X';
     for (i = 0; i < 12; i++) {
         document.getElementById('ttt' + i).innerHTML = '';
         document.getElementById('ttt' + i).disabled = false;
-        document.getElementById('ttt' + i).style.backgroundColor = '#00008B'
+        document.getElementById('ttt' + i).style.backgroundColor = '#00008B';
     }
 }
 
@@ -34,9 +36,11 @@ function hasThreeInARow() {
     isLine(2, 3, 0) || isLine(3, 0, 1) || isLine(4, 5, 6) ||
     isLine(5, 6, 7) || isLine(6, 7, 4) || isLine(7, 4, 5) ||
     isLine(8, 9, 10) || isLine(9, 10, 11) || isLine(10, 11, 8) ||
-    isLine(11, 8, 9) || isLine(0, 5, 10) || isLine(1, 6, 11) ||
-    isLine(2, 7, 8) || isLine(3, 4, 9) || isLine(0, 7, 10) ||
-    isLine(1, 4, 11) || isLine(2, 5, 8) || isLine(3, 6, 9))
+    isLine(11, 8, 9) || isLine(0, 4, 8) || isLine(1, 5, 9) ||
+    isLine(2, 6, 10) || isLine(3, 7, 11) || isLine(0, 5, 10) ||
+    isLine(1, 6, 11) || isLine(2, 7, 8) || isLine(3, 4, 9) ||
+    isLine(0, 7, 10) || isLine(1, 4, 11) || isLine(2, 5, 8) ||
+    isLine(3, 6, 9))
 }
 
 function disableAllSquares() {
