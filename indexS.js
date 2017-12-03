976b310eeaa7c27cd098444de4e3a6dc8d9b0372
Kevin Dong <dongkl@hotmail.com>
2017-12-02 19:12:55 -0500

    Added highlight to index.js

--- indexS.js
+++ indexS.js
@@ -3,18 +3,20 @@
 let numMoves = 0;
 const icons = ['X', 'O'];
 var grid = ['', '', '', '', '', '', '', '', '', '', '', ''] // 12 boxes
+const tests = []; //Stuff needs to be added here
+let toHighlight = [];
 
 function toMove(numMoves) {
     return icons[numMoves % 2];
 }
 
-function testLine(a, b, c) {
+function isLine(a, b, c) {
     return (grid[a] === grid[b] && grid[b] === grid[c] && grid[a] !== '')
 }
 
 function newGame() {
     numMoves = 0;
-    grid = ['', '', '', '', '', '', '', '', ''];
+    grid = ['', '', '', '', '', '', '', '', '', '', '', ''];
     document.getElementById('tttToMove').innerHTML = 'To Move: X';
     for (i = 0; i < 12; i++) {
         document.getElementById('ttt' + i).innerHTML = '';
@@ -23,13 +25,13 @@ function newGame() {
 }
 
 function hasThreeInARow() {
-    return (testLine(0, 1, 2) || testLine(1, 2, 3) ||
-    testLine(2, 3, 0) || testLine(3, 0, 1) || testLine(4, 5, 6) ||
-    testLine(5, 6, 7) || testLine(6, 7, 4) || testLine(7, 4, 5) ||
-    testLine(8, 9, 10) || testLine(9, 10, 11) || testLine(10, 11, 8) ||
-    testLine(11, 8, 9) || testLine(0, 5, 10) || testLine(1, 6, 11) ||
-    testLine(2, 7, 8) || testLine(3, 4, 9) || testLine(0, 7, 10) ||
-    testLine(1, 4, 11) || testLine(2, 5, 8) || testLine(3, 6, 9))
+    return (isLine(0, 1, 2) || isLine(1, 2, 3) ||
+    isLine(2, 3, 0) || isLine(3, 0, 1) || isLine(4, 5, 6) ||
+    isLine(5, 6, 7) || isLine(6, 7, 4) || isLine(7, 4, 5) ||
+    isLine(8, 9, 10) || isLine(9, 10, 11) || isLine(10, 11, 8) ||
+    isLine(11, 8, 9) || isLine(0, 5, 10) || isLine(1, 6, 11) ||
+    isLine(2, 7, 8) || isLine(3, 4, 9) || isLine(0, 7, 10) ||
+    isLine(1, 4, 11) || isLine(2, 5, 8) || isLine(3, 6, 9))
 }
 
 function disableAllSquares() {
